d6c02e17246d59958babfdbed9326ef595b7cd6c
Jacobo Martinez <jacobojavier98@gmail.com>
2019-07-29 14:29:18 -0400

    Delete client/ Readme; Fix body typo css; add process.env.PORT on server listening; update server side & client side package.json with most recent packages

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/api/customers', (req, res) => {
   res.json(customers);
 });
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(PORT, () => `Server running on port ${PORT}`);
